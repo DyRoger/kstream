a32e213f78972b1f1f3d287b0979c96b53382f3a
DyRoger <kaustabhadak30@gmail.com>
2024-02-12 00:04:08 +0530

    initial files

---
+++ src/main/java/com/dyroger/kstream/KafkaStreamProcessApplication.java
@@ -0,0 +1,13 @@
+package com.dyroger.kstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dyroger/kstream/KafkaStreamProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyroger.kstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
